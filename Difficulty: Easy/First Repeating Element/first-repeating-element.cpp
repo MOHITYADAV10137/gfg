69c47763414992e44cf5828d46e5fdeeffb05e27
MOHIT YADAV <151998891+MOHITYADAV10137@users.noreply.github.com>
2024-07-26 18:11:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    
+    int firstRepeated(vector<int>& arr) {
+        unordered_map<int, int> hash;
+
+    
+        for (int i = 0; i < arr.size(); i++) {
+            hash[arr[i]]++;
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            if (hash[arr[i]] > 1) {
+                return i + 1;  
+            }
+        }
+
+        return -1;  
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.firstRepeated(arr) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
