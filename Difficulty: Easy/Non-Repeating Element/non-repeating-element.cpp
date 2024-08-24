027f03dd6c61659fdb2bccbf3cf130cf8ccf74f6
MOHIT YADAV <151998891+MOHITYADAV10137@users.noreply.github.com>
2024-08-24 17:01:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non-Repeating Element/non-repeating-element.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int firstNonRepeating(vector<int>& arr) {
+        int n=arr.size();
+        unordered_map<int,int>map;
+        
+        for(int i=0;i<n;i++)
+        {
+            map[arr[i]]++;
+            
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(map[arr[i]]==1)
+            {
+                return arr[i];
+            }
+            
+        }
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        cout << ob.firstNonRepeating(nums) << endl;
+    }
+}
+
+// } Driver Code Ends
